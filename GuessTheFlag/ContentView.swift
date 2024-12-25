9d3a610b96b476f32715021da331013acf8712a5
Gabrielle Polite <gpolite@stanford.edu>
2024-12-25 05:26:43 -0500

    Add tapping a button func Includes an alert after a guess is made, introduce repeating functionality

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var COUNTRIES_LIST = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-    var correctAnswer = Int.random(in: 0...2)  // whether it is the first, second, or third flag
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)  // whether it is the first, second, or third flag
+    
+    // needed so we can update in flagTapped
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var body: some View {
         // needed to make the background blue
@@ -22,9 +26,9 @@ struct ContentView: View {
                 
                 // words
                 VStack {
-                    Text("Type the flag of")
+                    Text("Tap the flag of")
                         .foregroundStyle(.white)
-                    Text(COUNTRIES_LIST[correctAnswer])
+                    Text(countries[correctAnswer])  // name of correct country
                         .font(.largeTitle)
                         .fontWeight(.bold)
                         .foregroundStyle(.white)
@@ -33,16 +37,37 @@ struct ContentView: View {
                 // three flag pictures
                 ForEach(0..<3) { number in
                     Button {
-                        // when you press the button
+                        flagTapped(number)
                     } label: {
-                        Image(COUNTRIES_LIST[number])
+                        Image(countries[number])
                     }
                     
                 }
             }
         }
+        // button to keep the game going
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+    
+    // update the score when the user makes a guess
+    func flagTapped (_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Incorrect"
+        }
         
-
+        showingScore = true
+    }
+    
+    // ask another question to keep the game going
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
