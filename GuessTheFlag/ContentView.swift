a72fc0d43cd0ad0a9acf836bd4731a386b8ab282
Gabrielle Polite <gpolite@stanford.edu>
2024-12-25 05:11:52 -0500

    Create main page with flag options

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    var COUNTRIES_LIST = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)  // whether it is the first, second, or third flag
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // needed to make the background blue
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            // seperate words from flag pictures
+            VStack(spacing: 30) {
+                
+                // words
+                VStack {
+                    Text("Type the flag of")
+                        .foregroundStyle(.white)
+                    Text(COUNTRIES_LIST[correctAnswer])
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                }
+                
+                // three flag pictures
+                ForEach(0..<3) { number in
+                    Button {
+                        // when you press the button
+                    } label: {
+                        Image(COUNTRIES_LIST[number])
+                    }
+                    
+                }
+            }
         }
-        .padding()
+        
+
     }
 }
 
